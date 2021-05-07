119547229b4f2a5ce97f6a3f1abdc2eccde6b741
Harish Kumar <harishkumar9758@gmail.com>
2021-05-07 09:59:36 +0530

    App Test commit

---
+++ OperatingSystems/src/main/java/com/harish/operating/system/Process/App.java
@@ -0,0 +1,13 @@
+package com.harish.operating.system.Process;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
